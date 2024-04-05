660209d4d3b8d761fe415d8d01833c1fb774a5a0
gh.kang <gh.kang@cyrexpay.com>
2024-04-05 17:08:28 +0900

    init

---
+++ front/layouts/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router';
+
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
